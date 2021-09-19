8111745bc328b1a8cdfa2f33c6028cb4cfb5f95a
lblue <flwwsg@gmail.com>
2021-09-19 20:53:50 +0800

    add biggerZero

--- app.js
+++ app.js
@@ -25,12 +25,23 @@ module.exports = app => {
    * ```
    */
   app.validator = new Parameter(app.config.validate);
+  // 增加规则
   // 支持 json 类型
   app.validator.addRule('json', (rule, value) => {
     try {
       JSON.parse(value);
     } catch (err) {
       return 'must be json string';
+    }
+  });
+  // 大于 0 的整数
+  this.app.validator.addRule('biggerZero', (rule, value) => {
+    const v = parseInt(value);
+    if (isNaN(v)) {
+      return 'must integer';
+    }
+    if (v < 1) {
+      return 'integer must bigger 0';
     }
   });
 };
